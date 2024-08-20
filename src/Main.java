69a71fd0ffe5a73534614e33d906970c183439f5
jonathansoriano <jonathan.s.sanjuan@outlook.com>
2024-08-20 16:50:08 -0400

    Lets see if this works

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my vermin!!");
+        System.out.println("For some reason, my original project for GetClassy doesn't upload to GitHub");
     }
 }
